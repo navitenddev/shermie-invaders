72a676ff1e049bc1d84b78b5ed45d6e30035f4cb
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-02-21 10:14:55 -0500

    Refactor text styling

--- src/scenes/HowToPlay.js
+++ src/scenes/HowToPlay.js
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import { fontStyle } from '../utils/fontStyle.js';
 
 export class HowToPlay extends Scene {
     constructor() {
@@ -10,11 +11,9 @@ export class HowToPlay extends Scene {
         this.animatedBg.setOrigin(0.5, 0.5);
 
         this.add.image(512, 300, 'howToPlayLogo');
-        let gamefont = '"Press Start 2P", system-ui'
 
         this.add.text(512, 460, 'Use WASD to move Shermie and space bar to shoot! Have fun!', {
-            fontFamily: gamefont, fontSize: 30, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+            ...fontStyle,
             align: 'center',
             wordWrap: {
                 width: 600 
@@ -22,17 +21,12 @@ export class HowToPlay extends Scene {
         })
         .setOrigin(0.5);
 
-        this.backButton = this.add.text(512, 600, 'Back', {
-            fontFamily: gamefont, fontSize: 30, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        })
-        .setOrigin(0.5)
-        .setInteractive()
-        .on('pointerdown', () => {
-            this.scene.start('MainMenu');  
-        });
-
+        this.backButton = this.add.text(512, 600, 'Back', fontStyle)
+            .setOrigin(0.5)
+            .setInteractive()
+            .on('pointerdown', () => {
+                this.scene.start('MainMenu');  
+            });
     }
     update(){
         if (this.animatedBg) {
--- src/scenes/LevelSelect.js
+++ src/scenes/LevelSelect.js
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import { fontStyle } from '../utils/fontStyle.js';
 
 export class LevelSelect extends Scene {
     constructor() {
@@ -12,34 +13,25 @@ export class LevelSelect extends Scene {
         this.add.image(512, 300, 'levelSelectlogo');
         
         let spacing = 400;
-        let gamefont = '"Press Start 2P", system-ui'
         for (var i = 1; i <= 5; i++){
-            this.add.text(512, spacing, 'Level ' + i, {
-                fontFamily: gamefont, fontSize: 30, color: '#ffffff',
-                stroke: '#000000', strokeThickness: 8,
-                align: 'center'
-            })
+            this.add.text(512, spacing, 'Level ' + i, fontStyle)
+                .setOrigin(0.5)
+                .setInteractive()
+                .on('pointerdown', () => {
+                    this.scene.start('Game');  // Will change this to respective levels in the future
+                });
+            spacing += 50;
+        }
+                
+        spacing += 20;
+        this.backButton = this.add.text(512, spacing, 'Back', fontStyle)
             .setOrigin(0.5)
             .setInteractive()
             .on('pointerdown', () => {
-                this.scene.start('Game');  // Will change this to respective levels in the future
+                this.scene.start('MainMenu');  
             });
-            spacing += 50;
-        }
-        
-        spacing += 20;
-        this.backButton = this.add.text(512, spacing, 'Back', {
-            fontFamily: gamefont, fontSize: 30, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        })
-        .setOrigin(0.5)
-        .setInteractive()
-        .on('pointerdown', () => {
-            this.scene.start('MainMenu');  
-        });
-
     }
+
     update(){
         if (this.animatedBg) {
             this.animatedBg.tilePositionY += 1;
