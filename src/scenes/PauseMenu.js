867f1f180edc1887c02ac94779ac2992d685a44f
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-03-24 17:12:18 -0400

    Add relative spacing to pause menu

--- src/scenes/PauseMenu.js
+++ src/scenes/PauseMenu.js
@@ -11,6 +11,7 @@ export class PauseMenu extends Scene {
 
     create(data) {
         this.prev_scene = data.prev_scene;
+
         const boxWidth = 300;
         const boxHeight = 240;
         const boxX = (this.game.config.width - boxWidth) / 2;
@@ -21,15 +22,21 @@ export class PauseMenu extends Scene {
         graphics.fillRoundedRect(boxX, boxY, boxWidth, boxHeight, 10);
 
         this.sounds = this.registry.get('sound_bank');
-
         this.keys = InitKeyDefs(this);
+
+        const menuSpacing = 60;
+        let menuY = boxY + 60;
 
         this.resumeButton = this.add.text(0, 0, 'Resume', fonts.medium)
             .setOrigin(0.5)
             .setInteractive()
-            .on('pointerdown', () => { this.sounds.bank.sfx.click.play(); this.unpause(); })
-            .setPosition(boxX + boxWidth / 2, boxY + 60);
+            .on('pointerdown', () => {
+                this.sounds.bank.sfx.click.play();
+                this.unpause();
+            })
+            .setPosition(boxX + boxWidth / 2, menuY);
 
+        menuY += menuSpacing;
         this.quitButton = this.add.text(0, 0, 'Quit', fonts.medium)
             .setOrigin(0.5)
             .setInteractive()
@@ -40,16 +47,16 @@ export class PauseMenu extends Scene {
                 this.sounds.bank.music.ff7_fighting.stop();
                 this.sounds.bank.sfx.click.play();
                 this.sounds.bank.music.start.play();
-
                 this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
                     this.scene.stop('PauseMenu');
                     this.scene.stop(this.prev_scene);
                     this.scene.start('MainMenu');
                 });
             })
-            .setPosition(boxX + boxWidth / 2, boxY + 120);
+            .setPosition(boxX + boxWidth / 2, menuY);
 
         if (this.registry.get('debug_mode') === true) {
+            menuY += menuSpacing;
             this.cheatsButton = this.add.text(0, 0, 'Cheats', fonts.medium)
                 .setOrigin(0.5)
                 .setInteractive()
@@ -58,7 +65,7 @@ export class PauseMenu extends Scene {
                     this.scene.stop('PauseMenu');
                     this.scene.start('StatsMenu');
                 })
-                .setPosition(boxX + boxWidth / 2, boxY + 180);
+                .setPosition(boxX + boxWidth / 2, menuY);
         }
 
         this.keys.p.on('down', () => this.unpause());
