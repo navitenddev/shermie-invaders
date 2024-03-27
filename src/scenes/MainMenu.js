f915cdf1cd248b4a340762220c5ca46516104f95
hosua <hosua.o@gmail.com>
2024-03-26 20:16:37 -0400

    Revert "Please give this a test, fixed cut error on double button click and default background out of story"
    This reverts commit 8b1fb584497d134142e5c2dafc83fd8d5040323a.

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -48,19 +48,14 @@ export class MainMenu extends Scene {
         this.start_btn = this.add.text(512, menuY, 'PLAY', fonts.medium)
             .setOrigin(0.5)
             .setInteractive()
-        this.isTransitioning = false;
-
-        this.start_btn.on('pointerdown', () => {
-            if (this.isTransitioning) return;
-            this.isTransitioning = true;
-
-            this.sound.get('start').stop();
-            this.sounds.bank.sfx.win.play();
-            this.cameras.main.fadeOut(200, 0, 0, 0);
-            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
-                this.scene.start('Story');
+            .on('pointerdown', () => {
+                this.sound.get('start').stop();
+                this.sounds.bank.sfx.win.play();
+                this.cameras.main.fadeOut(200, 0, 0, 0);
+                this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
+                    this.scene.start('Story');
+                });
             });
-        });
 
         // Controls Button
         menuY += menuSpacing;
