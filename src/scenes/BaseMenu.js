f3f527367803f345d7e037f91d3fa6e70b208402
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-04-01 01:13:04 -0400

    Add shermie to main menu background

--- src/scenes/BaseMenu.js
+++ src/scenes/BaseMenu.js
@@ -17,26 +17,33 @@ export class BaseMenu extends Scene {
         this.add.rectangle(0, 0, this.game.config.width, this.game.config.height, 0x000000)
             .setOrigin(0, 0)
             .setDepth(0);
-
+    
         this.animatedBg = this.add.group();
-
         const numSprites = 50;
         this.spriteWidth = 60;
         this.spriteHeight = 60;
-
+    
         for (let i = 0; i < numSprites; i++) {
             const randomX = Phaser.Math.Between(0, this.game.config.width);
             const randomY = Phaser.Math.Between(0, this.game.config.height);
-            const randomFrame = Phaser.Math.Between(1, 18);
-
-            const sprite = this.add.sprite(randomX, randomY, `enemy${randomFrame}`)
-                .setOrigin(0.5, 0.5)
-                .setScale(.5)
-                .play(`enemy${randomFrame}_idle`);
-
+    
+            let sprite;
+            if (Math.random() < 0.33) {
+                sprite = this.add.sprite(randomX, randomY, 'shermie_bg')
+                    .setOrigin(0.5, 0.5)
+                    .setScale(0.5)
+                    .play('shermie_bg_idle');
+            } else {
+                const randomFrame = Phaser.Math.Between(1, 18);
+                sprite = this.add.sprite(randomX, randomY, `enemy${randomFrame}`)
+                    .setOrigin(0.5, 0.5)
+                    .setScale(0.5)
+                    .play(`enemy${randomFrame}_idle`);
+            }
+    
             this.animatedBg.add(sprite);
         }
-
+    
         const overlayColor = 0x000000;
         const overlayAlpha = 0.6;
         this.add.rectangle(0, 0, this.game.config.width, this.game.config.height, overlayColor, overlayAlpha)
@@ -69,7 +76,12 @@ export class BaseMenu extends Scene {
                 sprite.y += 1;
                 if (sprite.y > this.game.config.height) {
                     sprite.y = -this.spriteHeight;
-                    sprite.setTexture(`enemy${Phaser.Math.Between(1, 22)}`);
+                    
+                    if (Math.random() < 0.33) {
+                        sprite.setTexture('shermie_bg');
+                    } else {
+                        sprite.setTexture(`enemy${Phaser.Math.Between(1, 22)}`);
+                    }
                 }
             });
         }
