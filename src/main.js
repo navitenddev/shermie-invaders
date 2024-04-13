aabe6b97a4f79be0e92fce81f88412454a8a0303
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-04-12 21:06:19 -0400

    Restore totalHits increment in collision event

--- src/main.js
+++ src/main.js
@@ -106,6 +106,7 @@ export function init_collision_events(scene, scene_key) {
     scene.physics.add.overlap(scene.objs.bullets.player, scene.objs.enemies.grid, (player_bullet, enemy) => {
         if (scene.player_vars.power == "pierce" || scene.player_vars.perm_power.includes("pierce")) player_bullet.hurt_bullet();
         else player_bullet.deactivate();
+        scene.objs.player.totalHits++;
         enemy.die();
         if (scene.scoreManager) {
             scene.scoreManager.addScore(Math.round(enemy.scoreValue * scene.level));
