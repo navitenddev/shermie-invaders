33c535e36e624d1ab8af57e52f987f5d82800517
hosua <hosua.o@gmail.com>
2024-04-18 03:46:40 -0400

    Added touch gesture for mobile
    works on my phone, hope it works on the others

--- src/ui/list_container.js
+++ src/ui/list_container.js
@@ -22,39 +22,48 @@ class ListContainer extends Phaser.GameObjects.Container {
         this.border
             .lineStyle(2, border_color, 1)
             .strokeRect(-(w / 2), -(h / 2), w, h);
-        this.display_text = this.scene.add.bitmapText(0, 0,
-            fonts.small.fontName, "", fonts.small.size, 'left');
 
         this.title = this.scene.add.bitmapText(0, 0, fonts.small.fontName, title, fonts.small.size, 'Center');
         this.title.setPosition(-(this.title.width / 2), -(h / 2) + ListContainer.MARGINS);
-        this.display_text
-            .setPosition(-(this.display_text.width / 2), -(this.display_text.height / 2));
+        this.display_text = this.scene.add.bitmapText(0, 0,
+            fonts.small.fontName, "", fonts.small.size, 'left');
         this.entries = entries
 
         this.bg
-            .setInteractive()
+            .setInteractive({ draggable: true })
             .on('wheel', (pointer, dx, dy, dz) => {
                 if (dy > 0)
                     this.#scroll_down();
                 else if (dy < 0)
                     this.#scroll_up();
+            })
+            .on('drag', (pointer, dx, dy) => {
+                if (dy > 0)
+                    this.#scroll_up();
+                else if (dy < 0)
+                    this.#scroll_down();
             });
 
         this.arrow_up = this.scene.add.image(0, -(this.display_text.height / 2) - ListContainer.ARROW_OFFSET, 'arrow')
             .setScale(0.5)
             .setVisible(false);
+        // .setInteractive()
+        // .on('pointerdown', () => { this.#scroll_up(); });
         this.arrow_down = this.scene.add.image(0, +(this.display_text.height / 2) + ListContainer.ARROW_OFFSET, 'arrow')
             .setAngle(180)
             .setScale(0.5);
+        // .setInteractive()
+        // .on('pointerdown', () => { this.#scroll_down(); });
 
-        this.arrow_up.setPosition(0, -(this.display_text.height / 2) - ListContainer.ARROW_OFFSET);
 
         if (this.#scroll_y + ListContainer.MAX_LINES_DISPLAYED < this.entries.length)
             this.arrow_down.setVisible(false);
         this.#update_text();
 
         this.add([this.bg, this.border, this.title, this.display_text, this.arrow_up, this.arrow_down]);
+        this.arrow_up.setPosition(0, -(this.display_text.height / 2) - ListContainer.ARROW_OFFSET);
         this.arrow_down.setPosition(0, +(this.display_text.height / 2) + ListContainer.ARROW_OFFSET);
+        this.display_text.setPosition(-(this.display_text.width / 2), -(this.display_text.height / 2));
     }
 
     #scroll_down() {
@@ -88,25 +97,16 @@ class ListContainer extends Phaser.GameObjects.Container {
         }
 
         if (this.#scroll_y === 0)
-            this.arrow_up
-                .setVisible(false)
-                .setPosition(0, -(this.display_text.height / 2) - ListContainer.ARROW_OFFSET);
+            this.arrow_up.setVisible(false);
         else
-            this.arrow_up
-                .setVisible(true)
-                .setPosition(0, -(this.display_text.height / 2) - ListContainer.ARROW_OFFSET);
+            this.arrow_up.setVisible(true);
 
         if (this.#scroll_y + ListContainer.MAX_LINES_DISPLAYED < this.entries.length)
-            this.arrow_down
-                .setVisible(true)
-                .setPosition(0, +(this.display_text.height / 2) + ListContainer.ARROW_OFFSET);
+            this.arrow_down.setVisible(true);
         else
-            this.arrow_down
-                .setVisible(false)
-                .setPosition(0, +(this.display_text.height / 2) + ListContainer.ARROW_OFFSET);
+            this.arrow_down.setVisible(false);
 
-        this.display_text.setText(output)
-            .setPosition(-(this.display_text.width / 2), -(this.display_text.height / 2));
+        this.display_text.setText(output);
     }
 }
 
