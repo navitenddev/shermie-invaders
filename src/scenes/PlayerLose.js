d4685ec2e5c9c3b80a7282e0df2ba861f00de5d0
hosua <hosua.o@gmail.com>
2024-04-18 16:55:45 -0400

    Added stats container to player lose scene

--- src/scenes/PlayerLose.js
+++ src/scenes/PlayerLose.js
@@ -5,6 +5,8 @@ import { start_dialogue } from './Dialogue.js';
 import { restart_scenes } from '../main.js';
 import { TextboxButton } from '../ui/textbox_button.js';
 import { ListContainer } from '../ui/list_container.js';
+import { StatsContainer } from '../ui/stats_container.js';
+
 
 export class PlayerLose extends Scene {
     emitter = EventDispatcher.getInstance();
@@ -30,7 +32,7 @@ export class PlayerLose extends Scene {
         const games_played = parseInt(localStorage.getItem('games_played')) || 1;
 
         // only save score if we started level 1, or started with no money
-        if (this.registry.get('valid_hiscore')) {
+        if (this.registry.get('valid_hiscore') === true) {
             game_hiscores.push(`#${games_played} ${score}`);
         }
         game_hiscores.sort((og_a, og_b) => {
@@ -44,10 +46,38 @@ export class PlayerLose extends Scene {
                 return 0;
         });
 
+        const text_gameover = this.add.bitmapText(
+            0,
+            10,
+            fonts.medium.fontName,
+            "GAME OVER",
+            fonts.medium.size,
+            'center'
+        );
+
+        text_gameover.setPosition(
+            (this.game.config.width / 2) - (text_gameover.width / 2), 
+            10
+        );
+
+        const text_score = this.add.bitmapText(
+            0, 
+            50,
+            fonts.middle.fontName,
+            `FINAL SCORE: ${score}`,
+            fonts.middle.size,
+            'center'
+        );
+
+        text_score.setPosition(
+            (this.game.config.width / 2) - (text_score.width / 2),
+            40
+        );
+
         localStorage.setItem('game_hiscores', JSON.stringify(game_hiscores));
 
         game_hiscores = game_hiscores.map((s, i) => { return `${i + 1} ${s}`; });
-        new ListContainer(this, 325, 200, 350, 350, game_hiscores, "Hiscores", 8);
+        new ListContainer(this, 325, 240, 350, 325, game_hiscores, "Hiscores", 8);
 
         this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_IN_COMPLETE, () => {
             start_dialogue(this.scene, `techtip${rand_idx}`, "techtip");
@@ -55,71 +85,7 @@ export class PlayerLose extends Scene {
 
         this.emitter.removeAllListeners();
 
-        // const titleText = this.add.bitmapText(
-        //     this.game.config.width / 2,
-        //     100,
-        //     fonts.large.fontName,
-        //     'GAME OVER',
-        //     fonts.large.size
-        // );
-        // titleText.setOrigin(0.5);
-
-        // this.final_score = this.add.bitmapText(
-        //     this.game.config.width / 2,
-        //     titleText.y + titleText.height + 50,
-        //     fonts.medium.fontName,
-        //     `FINAL SCORE: ${score}`,
-        //     fonts.medium.size
-        // );
-        // this.final_score.setOrigin(0.5);
-
-        /**
-         * This REALLY should be made into a phaser container.
-         * Josh, do not push this. Make it good
-         */
-        // const stats_x = 700, stats_y = 150,
-        //     stats_w = 290, stats_h = 150;
-        // const { shots_fired, shots_hit } = this.player_vars.game_stats;
-        // const hitMissRatio = shots_hit / (shots_fired || 1);
-
-        // const statsSpacing = 35;
-
-        // const shotsFiredText = this.add.bitmapText(
-        //     -(stats_w / 2),
-        //     0,
-        //     fonts.small.fontName,
-        //     `SHOTS FIRED: ${shots_fired}`,
-        //     fonts.small.size
-        // );
-        // shotsFiredText
-        //     .setTint(0xade6ff);
-        // const hitsText = this.add.bitmapText(
-        //     -(stats_w / 2),
-        //     1 * statsSpacing,
-        //     fonts.small.fontName,
-        //     `HITS: ${shots_hit}`,
-        //     fonts.small.size
-        // );
-        // const hitMissRatioText = this.add.bitmapText(
-        //     -(stats_w / 2),
-        //     statsSpacing * 2,
-        //     fonts.small.fontName,
-        //     `HIT-MISS RATIO: ${(hitMissRatio * 100).toFixed(0)}%`,
-        //     fonts.small.size
-        // );
-        // hitMissRatioText.setTint(0xe0de2c);
-
-        // const stats_bg = this.add.rectangle(0, 0, stats_w, stats_h, 0x2B2D31);
-        // const stats_border = this.add.graphics();
-
-        // stats_border.lineStyle(2, 0x879091, 1)
-        //     .strokeRect(-(stats_w / 2), -(stats_h / 2), stats_w, stats_h);
-
-        // // please make this its own object
-        // const stats_container = this.add.container(
-        //     stats_x, stats_y,
-        //     [stats_bg, stats_border, shotsFiredText, hitsText, hitMissRatioText]
-        // );
+        new StatsContainer(this, 700, 240, 350, 325);
 
         this.continue_btn = new TextboxButton(
             this,
